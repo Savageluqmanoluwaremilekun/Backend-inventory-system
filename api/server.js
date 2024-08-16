e860e775c650443c4289447b7adaa4fa9f45cf7b
savageluqmanoluwaremilekun <savageluqman123@gmail.com>
2024-08-16 12:19:12 +0100

    first commit

--- api/server.js
+++ api/server.js
@@ -1,6 +1,6 @@
 const http = require("http");
-const app = require("./src/app");
-const connectDB = require("./src/database/connectDB");
+const app = require("../src/app");
+const connectDB = require("../src/database/connectDB");
 require("dotenv").config();
 const PORT = process.env.PORT || 3000;
 
